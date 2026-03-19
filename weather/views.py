5a42a1edeb6992159150e26a173ef9ec22bc0b25
Andrés Prado Morgaz <andpramordev@gmail.com>
2026-03-19 19:29:26 +0100

    Data table

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,8 @@
+import csv
+import os
+from datetime import datetime
+
+from django.conf import settings
 from django.shortcuts import render
 
 
@@ -5,9 +10,32 @@ def index(request):
     return render(request, "tiempo/index.html")
 
 
-def datos(request):
-    return render(request, "tiempo/datos.html")
-
-
 def graficos(request):
     return render(request, "tiempo/graficos.html")
+
+
+def datos(request):
+    datos_clima = []
+    ruta_archivo = os.path.join(
+        settings.BASE_DIR, "data_silver_layer/Tiempo_por_horas.csv"
+    )
+
+    traduccion = {
+        "Sunny": "Soleado",
+        "Partly Sunny": "Parcialmente soleado",
+        "Overcast": "Nublado",
+        "Cloudy": "Nublado",
+        "Rain": "Lluvia",
+    }
+
+    with open(ruta_archivo, mode="r", encoding="utf-8") as f:
+        lector = csv.DictReader(f)
+        for fila in lector:
+            try:
+                fila["date"] = datetime.fromisoformat(fila["date"])
+                fila["summary"] = traduccion.get(fila["summary"], fila["summary"])
+            except ValueError:
+                pass
+            datos_clima.append(fila)
+
+    return render(request, "tiempo/datos.html", {"datos": datos_clima})
