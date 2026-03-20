b4d49432506482ea78acc4a2b506f692504ed76b
Andres Prado Morgaz <andpramordev@gmail.com>
2026-03-20 11:57:01 +0100

    Añado TO-DO

--- weather/models.py
+++ weather/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class WeatherData(models.Model):
     timestamp_captura = models.DateTimeField(auto_now_add=True)
     payload = models.JSONField()  # Almacena el JSON de OpenMeteo directamente
+    # TODO: cambiar esto por un modelo de verdad, que luego queremos paginar y cosas así, en vez de almacenar el JSON entero, almacenar solo los campos que nos interesan.
 
     class Meta:
         db_table = "openmeteo"
