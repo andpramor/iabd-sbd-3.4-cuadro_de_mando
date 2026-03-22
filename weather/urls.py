4dc3c1334f89036e0619bd48ef8063cb84752eef
Andres Prado Morgaz <andpramordev@gmail.com>
2026-03-22 10:11:37 +0100

    Formato fechas

--- weather/urls.py
+++ weather/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import *
+from .views import datos_hourly, graficos, index
 
 urlpatterns = [
     path("", index, name="index"),
-    path("datos/<str:provincia>/", datos, name="datos"),
+    path("datos/<str:provincia>/", datos_hourly, name="datos"),
     path("graficos/", graficos, name="graficos"),
 ]
--- weather/views.py
+++ weather/views.py
@@ -15,7 +15,7 @@ def graficos(request):
     return render(request, "tiempo/graficos.html")
 
 
-def datos(request, provincia):
+def datos_hourly(request, provincia):
     datos_clima = []
     nombre_archivo = f"{provincia}.csv"
     ruta_archivo = os.path.join(
@@ -39,23 +39,21 @@ def datos(request, provincia):
             lector = csv.DictReader(f)
             for fila in lector:
                 try:
-                    fila["date"] = datetime.fromisoformat(fila["date"])
+                    fila["date"] = datetime.fromisoformat(fila["date"]).strftime(
+                        "%d/%m/%Y - %H:%M"
+                    )  # Antes no hacía el .strftime().
                     fila["summary"] = traduccion.get(fila["summary"], fila["summary"])
                 except ValueError:
                     pass
                 datos_clima.append(fila)
 
-    
     # Mostramos 24 registros por página (1 día cada hora)
-    paginator = Paginator(datos_clima, 24) 
-    
+    paginator = Paginator(datos_clima, 24)
+
     # Obtenemos el número de página de la URL (ej: /datos/sevilla/?page=2)
-    numero_pagina = request.GET.get('page')
+    numero_pagina = request.GET.get("page")
     page_obj = paginator.get_page(numero_pagina)
 
-    context = {
-        "datos": page_obj,
-        "provincia": provincia
-    }
+    context = {"datos": page_obj, "provincia": provincia}
 
     return render(request, "tiempo/datos.html", context)
